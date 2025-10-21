072d823ada4b1771ff21387137c74a7e7b8f8afa
Carl Borillo <78941313+CarlBorillo@users.noreply.github.com>
2025-10-21 16:16:14 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/g4U0rkjY)
+# project-1-balance-telegram
+Project 1
+
+Group members:
+
+Ada Lovelace adalovelace@csu.fullerton.edu
+Charles Babbage charlesbab@csu.fullerton.edu
---
+++ algorithms.cpp
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////
+// algorithms.cpp
+//
+// Definitions for three algorithms:
+//
+// find_dip
+// longest_balanced_span
+// telegraph_style
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "algorithms.hpp"
+
+std::vector<int>::const_iterator algorithms::find_dip(const std::vector<int>& values) {
+
+  // TODO: Rewrite the body of this function so that it actually works. That
+  // includes rewriting the return statement. After you do that, delete this
+  // comment.
+  return values.end();
+}
+
+std::optional<algorithms::span> algorithms::longest_balanced_span(const std::vector<int>& values) {
+
+  // TODO: Rewrite the body of this function so that it actually works. That
+  // includes rewriting the return statement. After you do that, delete this
+  // comment.
+  return std::nullopt;
+}
+
+std::string algorithms::telegraph_style(const std::string& s) {
+
+  // TODO: Rewrite the body of this function so that it actually works. That
+  // includes rewriting the return statement. After you do that, delete this
+  // comment.
+  return "";
+}
